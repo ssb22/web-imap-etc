441c8a109a0248ec41c5df3db4f8bf94e46a5809
Silas S. Brown <ssb22@cam.ac.uk>
2017-05-29 11:44:07 +0000

    Update Annotator Generator, WebCheck, css-generate.py
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/setup@2794 29193198-4895-4776-b068-10539e920549

--- webcheck.py
+++ webcheck.py
@@ -101,7 +101,7 @@ class HTMLStrings(HTMLParser.HTMLParser):
         if self.theTxt and self.theTxt[-1].endswith(thing): return
         self.theTxt.append(thing)
     def handle_starttag(self, tag, attrs):
-        if tag in "p br div h1 h2 h3 h4 h5 h6 tr td table".split(): self.ensure(' ') # space rather than newline because we might want to watch for a string that goes across headings etc
+        if tag in "p br div h1 h2 h3 h4 h5 h6 th tr td table".split(): self.ensure(' ') # space rather than newline because we might want to watch for a string that goes across headings etc
     def handle_startendtag(self, tag, attrs):
         self.handle_starttag(tag,attrs)
     def unescape(self,attr): return attr # as we don't use attrs above, no point trying to unescape them and possibly falling over if something's malformed
