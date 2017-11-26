03a13b92711c825e15fe04288adde65909c69d41
Silas S. Brown <ssb22@cam.ac.uk>
2017-11-26 03:01:28 -0600

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -2,4 +2,4 @@
 wget -N http://people.ds.cam.ac.uk/ssb22/setup/maclinux.txt
 wget -N http://people.ds.cam.ac.uk/ssb22/setup/webcheck.py
 wget -N http://people.ds.cam.ac.uk/ssb22/setup/imapfix.py
-git commit -am update && git push
+git commit -am "Update $(echo $(git diff|grep '^--- a/'|sed -e 's,^--- a/,,')|sed -e 's/ /, /g')" && git push
