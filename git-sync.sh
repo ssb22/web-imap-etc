07f273e41404cab38aaee2ebef7d1cf0144e95d2
Silas S. Brown <ssb22@hermes.cam.ac.uk>
2019-06-25 13:38:23 +0100

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git pull --no-edit
 wget -N http://people.ds.cam.ac.uk/ssb22/setup/maclinux.txt http://people.ds.cam.ac.uk/ssb22/setup/webcheck.py http://people.ds.cam.ac.uk/ssb22/setup/imapfix.py http://people.ds.cam.ac.uk/ssb22/gradint/timetrack.js
 git commit -am "Update $(echo $(git diff|grep '^--- a/'|sed -e 's,^--- a/,,')|sed -e 's/ /, /g')" && git push
