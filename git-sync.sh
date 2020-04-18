840cb047c6f26bc0f76e6b76e330b571c1dacc85
Silas S. Brown <ssb22@cam.ac.uk>
2020-04-18 09:07:11 +0100

    Remove git-sync.sh (updates now done by homepage/Makefile itself)

--- git-sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git pull --no-edit
-wget -N http://ssb22.user.srcf.net/setup/maclinux.txt http://ssb22.user.srcf.net/setup/webcheck.py http://ssb22.user.srcf.net/setup/imapfix.py http://ssb22.user.srcf.net/gradint/timetrack.js
-git commit -am "Update $(echo $(git diff|grep '^--- a/'|sed -e 's,^--- a/,,')|sed -e 's/ /, /g')" && git push
