080d157f746144c0ea6e7c08ad64a9428a7cf8d0
Silas S. Brown <ssb22@cam.ac.uk>
2021-02-27 07:40:30 +0000

    more debugging information on Javascript failures

--- webcheck.py
+++ webcheck.py
@@ -398,7 +398,7 @@ def run_webdriver_inner(actionList,browser):
             tries = 30
             while tries and not myFind(a[1:-1],getSrc()):
               time.sleep(delay) ; tries -= 1
-            if not tries: raise NoTracebackException("webdriver timeout while waiting for \"%s\", current URL is \"%s\"\n" % (a[1:-1],browser.current_url)) # don't put (current URL is "") in parentheses: if the resulting email is viewed in (at least some versions of) MHonArc, a bug can result in &quot (i.e. &quot; without the ;) being added to the href, with the ; occurring after the </a>
+            if not tries: raise NoTracebackException("webdriver timeout while waiting for \"%s\", current URL is %s content \"%s\"\n" % (a[1:-1],browser.current_url,getSrc())) # don't quote current URL: if the resulting email is viewed in (at least some versions of) MHonArc, a bug can result in &quot being added to the href
         elif a.startswith('[') and a.endswith(']'): # click
             findElem(a[1:-1]).click()
         elif a.startswith('/') and '/' in a[1:]: # click through items in a list to reveal each one (assume w/out Back)
