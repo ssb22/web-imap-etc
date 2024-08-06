6bdde25799c0a05b5e98efafe5b5fe0cb1d3b879
Silas S. Brown <ssb22@cam.ac.uk>
2024-08-06 06:52:34 +0100

    gallery MarkDown option

--- gallery.sh
+++ gallery.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Create quick index.html from pictures directory
-# Silas S. Brown - public domain
+# Silas S. Brown - public domain - v1.1
 
 # use with (e.g.) webfsd -f index.html   # port 8000
 
-if test -e index.html; then
-    echo "Error: index.html already exists"
+# or use --md to create README.md for GitHub/GitLab
+
+if [ "$1" == --md ] ; then Out=README.md; else Out=index.html; fi
+if [ -e "$Out" ]; then
+    echo "Error: $Out already exists"
     exit 1
 fi
-(echo '<html><body style="overflow-x:hidden;margin:0">';
-for F in *.jpg *.JPG; do if test -e "$F"; then
+if [ "$Out" == index.html ] ; then echo '<html><body style="overflow-x:hidden;margin:0">' > "$Out"; fi
+for F in *; do case $F in *.jpg|*.JPG|*.png|*.PNG)
+    case "$Out" in
+        (README.md) echo '!'"[]($F)" ;;
+        (index.html)
   case "$(exiftool -list "$F"|grep ^Orientation|sed -e 's/.*: //')" in
       ("Rotate 90 CW")
           echo "<div style=\"display:table\"><div style=\"padding:50% 0;height:0\"><img style=\"width:auto;height:calc(100vw - 31px);display:block;transform-origin: top left; transform: rotate(90deg) translate(0,-100%) ; margin-top: -50%; image-orientation: none\" src=\"$F\"></div></div>"
@@ -19,7 +25,10 @@ for F in *.jpg *.JPG; do if test -e "$F"; then
       # The image-orientation part is to turn off the automatic EXIF reading in newer browsers, because we're supplying it for older browsers
       (*)
           echo "<img style=\"width:100%; height:auto;\" src=\"$F\">"
-         ;;
-      esac
+          ;;
+      esac # rotated or not
+      ;;
+    esac # md / html
   echo "$F" >&2
-fi; done ) > index.html && du -h index.html && exit
+;; esac; done >> "$Out"
+du -h "$Out"
